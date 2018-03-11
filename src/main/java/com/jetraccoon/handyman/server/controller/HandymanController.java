fcfdf5524d6dbffb3380a5c112336731812d9f80
Ramazan Akhmedov <ramazan.akhmedov@gmail.com>
2018-03-11 12:49:11 +0300

    Изменил на RestControllee

--- src/main/java/com/jetraccoon/handyman/server/controller/HandymanController.java
+++ src/main/java/com/jetraccoon/handyman/server/controller/HandymanController.java
@@ -5,13 +5,13 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/handyman")
 public class HandymanController {
 
     @RequestMapping(value = "/get", method = RequestMethod.GET)
-    @ResponseBody
     public String getName(ModelMap model){
         return "RUFET";
     }
