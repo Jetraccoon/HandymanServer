e8ff3ae7b0627151d1064828f308935cca87ba5c
Ramazan Akhmedov <ramazan.akhmedov@gmail.com>
2018-03-17 13:43:43 +0300

    закончил модуль подключения к БД

--- src/main/java/com/jetraccoon/handyman/server/config/DatabaseConfig.java
+++ src/main/java/com/jetraccoon/handyman/server/config/DatabaseConfig.java
@@ -7,11 +7,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.annotation.Resource;
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 import java.io.IOException;
 import java.io.InputStream;
@@ -45,7 +48,24 @@ public class DatabaseConfig {
         ds.setUsername(env.getRequiredProperty("db.username"));
         ds.setPassword(env.getRequiredProperty("db.password"));
 
+        ds.setInitialSize(Integer.valueOf(env.getProperty("db.initialSize")));
+        ds.setMinIdle(Integer.valueOf(env.getProperty("db.minIdle")));
+        ds.setMaxIdle(Integer.valueOf(env.getProperty("db.maxIdle")));
+        ds.setTimeBetweenEvictionRunsMillis(Long.valueOf(env.getRequiredProperty("db.timeBetweenEvictionRunsMillis")));
+        ds.setMinEvictableIdleTimeMillis(Long.valueOf(env.getRequiredProperty("db.minEvictableIdleTimeMillis")));
+        ds.setTestOnBorrow(Boolean.valueOf(env.getRequiredProperty("db.testOnBorrow")));
+        ds.setValidationQuery(env.getRequiredProperty("db.validationQuery"));
+
+
         return ds;
+    }
+
+    @Bean
+    public PlatformTransactionManager platformTransactionManager(){
+        JpaTransactionManager manager = new JpaTransactionManager();
+        manager.setEntityManagerFactory(entityManagerFactoryBean().getObject());
+
+        return manager;
     }
 
     public Properties getHibernateProperties() {
