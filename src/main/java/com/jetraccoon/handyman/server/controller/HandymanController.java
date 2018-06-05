23c294804b3c03666393a5a5011a582b28e85ffa
Ramazan Akhmedov <ramazan.akhmedov@gmail.com>
2018-06-05 19:15:50 +0300

    Закончил контроллер

--- src/main/java/com/jetraccoon/handyman/server/controller/HandymanController.java
+++ src/main/java/com/jetraccoon/handyman/server/controller/HandymanController.java
@@ -3,10 +3,10 @@ package com.jetraccoon.handyman.server.controller;
 import com.jetraccoon.handyman.server.entity.Cunsomer;
 import com.jetraccoon.handyman.server.repository.CunsomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @RestController
@@ -20,18 +20,21 @@ public class HandymanController {
     }
     @RequestMapping(value = "/get/{id}", method = RequestMethod.GET)
     public Cunsomer getCunsomer(@PathVariable long id) {
-        return cunsomerRepository.getOne(id);
+        return cunsomerRepository.findById(id).get();
     }
 
-    @RequestMapping(
-            value = {"/create"},
-            method = {RequestMethod.GET}
+    @RequestMapping(value = {"/create"},method = {RequestMethod.GET}
     )
     public String createCunsomer(@RequestParam("name") String name, @RequestParam("title") String title) {
         Cunsomer cunsomer = new Cunsomer();
         cunsomer.setName(name);
         cunsomer.setTitle(title);
-        this.cunsomerRepository.saveAndFlush(cunsomer);
+        cunsomerRepository.saveAndFlush(cunsomer);
         return "Success";
+    }
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    public String deleteCunsomer(@PathVariable long id) {
+        cunsomerRepository.deleteById(id);
+        return "Success deleted cunsomer id="+ id;
     }
 }
